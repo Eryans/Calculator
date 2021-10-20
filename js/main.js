a2a6f31887690e50e8d2c7291826d4ab777126fa
Julio <julesnoir@hotmail.fr>
2021-10-20 17:26:31 +0200

    Added Project Content

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const buttonsNum = document.querySelectorAll(".btnNumber");
+const buttonsFunc = document.querySelectorAll(".btnFunc");
+const textArea = document.querySelector("#calculus");
+const btnReset = document.querySelector(".btnReset");
+const btnDel = document.querySelector(".btnDel");
+const btnResult = document.querySelector(".btnResult");
+
+let operationToDo = "";
+let previousvalue = 0;
+
+window.onload = () => textArea.value = "";
+
+buttonsNum.forEach( x => x.addEventListener("click",function(){textArea.value += this.value}) );
+
+btnReset.addEventListener("click",function(){
+    textArea.value = "";
+    previousvalue = 0;
+    textArea.placeholder = "";
+});
+btnDel.addEventListener("click",function(){
+    if (textArea.value.length != 0){
+        let str = textArea.value.toString().split("");
+        str.pop();
+        textArea.value = parseFloat(str.join(""));
+    }
+});
+buttonsFunc.forEach( x => x.addEventListener("click",function(){
+    previousvalue += parseFloat(textArea.value);
+    textArea.value = "";
+    textArea.placeholder = previousvalue;
+    operationToDo = this.value;
+}));
+
+btnResult.addEventListener("click",function(){
+    switch (operationToDo){
+        case "Add" :
+        textArea.value = parseFloat(previousvalue) + parseFloat(textArea.value);
+        break;
+        case "Minus" :
+        textArea.value = parseFloat(previousvalue) - parseFloat(textArea.value);
+        break;
+        case "Divide" :
+        textArea.value = parseFloat(previousvalue) / parseFloat(textArea.value);
+        break;
+        case "Multiply" :
+        textArea.value = parseFloat(previousvalue) * parseFloat(textArea.value);
+        break;
+    }
+});
